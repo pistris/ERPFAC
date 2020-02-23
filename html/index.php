fc7e25010053d404cdeb3c12279b47fb689717f7
Davidfm14 <61220173+Davidfm14@users.noreply.github.com>
2020-02-23 00:58:38 -0600

    CamelCase en nombres de archivos, Estructura del proyecto, push master

--- html/index.php
+++ html/index.php
@@ -13,10 +13,10 @@
   <title>Administración de categorías</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <script src="js/jquery.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="SCR/css/bootstrap.min.css">
+  <script src="SCR/js/jquery.min.js"></script>
+  <script src="SCR/js/popper.min.js"></script>
+  <script src="SCR/js/bootstrap.min.js"></script>
 </head>
 
 <body>
