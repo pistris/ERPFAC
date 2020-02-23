8c5a9168a3d3a51285c8a06055a90ee0b6e5ba3c
Davidfm14 <61220173+Davidfm14@users.noreply.github.com>
2020-02-23 02:26:06 -0600

    dashboar, header y footer

---
+++ html/dashboard.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html> 
+<!-- 
+Template Name: Marvin - Responsive Bootstrap 4 Admin Dashboard Template
+Author: Hencework
+Contact: https://hencework.ticksy.com/
+
+License: You must have a valid license purchased only from templatemonster to legally use the template for your project.
+-->
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>Marvin I CRM Dashboard</title>
+    <meta name="description" content="A responsive bootstrap 4 admin dashboard template by hencework" />
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+	
+	<!-- vector map CSS -->
+    <link href="../vendors/vectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" type="text/css" />
+	
+	<link href="../vendors/apexcharts/dist/apexcharts.css" rel="stylesheet" type="text/css" />
+	
+    <!-- Toggles CSS -->
+    <link href="../vendors/jquery-toggles/css/toggles.css" rel="stylesheet" type="text/css">
+    <link href="../vendors/jquery-toggles/css/themes/toggles-light.css" rel="stylesheet" type="text/css">
+	
+	<!-- Toastr CSS -->
+    <link href="../vendors/jquery-toast-plugin/dist/jquery.toast.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <!-- Preloader 
+    <div class="preloader-it">
+        <div class="loader-pendulums"></div>
+    </div>
+    --><!-- /Preloader -->
+	
+    <!-- HK Wrapper -->
+    <div class="hk-wrapper hk-vertical-nav">
+        <?php include('Templates/header.php'); ?>
+    
+        <!-- Main Content -->
+        <div class="hk-pg-wrapper">
+           
+
+            <!-- Footer -->
+            <?php include('Templates/footer.php'); ?>
+            <!-- /Footer -->
+
+        </div>
+        <!-- /Main Content -->
+    </div>
+    <!-- /HK Wrapper -->
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Slimscroll JavaScript -->
+    <script src="../dist/js/jquery.slimscroll.js"></script>
+
+    <!-- Fancy Dropdown JS -->
+    <script src="../dist/js/dropdown-bootstrap-extended.js"></script>
+
+    <!-- FeatherIcons JavaScript -->
+    <script src="../dist/js/feather.min.js"></script>
+
+    <!-- Toggles JavaScript -->
+    <script src="../vendors/jquery-toggles/toggles.min.js"></script>
+    <script src="../dist/js/toggle-data.js"></script>
+	
+	<!-- Counter Animation JavaScript -->
+	<script src="../vendors/waypoints/lib/jquery.waypoints.min.js"></script>
+	<script src="../vendors/jquery.counterup/jquery.counterup.min.js"></script>
+	
+	<!-- Morris Charts JavaScript -->
+    <script src="../vendors/raphael/raphael.min.js"></script>
+    <script src="../vendors/morris.js/morris.min.js"></script>
+	
+	<!-- EChartJS JavaScript -->
+    <script src="../vendors/echarts/dist/echarts-en.min.js"></script>
+    
+	<!-- Sparkline JavaScript -->
+    <script src="../vendors/jquery.sparkline/dist/jquery.sparkline.min.js"></script>
+	
+	<!-- Vector Maps JavaScript -->
+    <script src="../vendors/vectormap/jquery-jvectormap-2.0.3.min.js"></script>
+    <script src="../vendors/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="../dist/js/vectormap-data.js"></script>
+
+
+	<!-- Owl JavaScript -->
+    <script src="../vendors/owl.carousel/dist/owl.carousel.min.js"></script>
+	
+	<!-- Toastr JS 
+    <script src="../vendors/jquery-toast-plugin/dist/jquery.toast.min.js"></script>-->
+    
+    <!-- Init JavaScript -->
+    <script src="../dist/js/init.js"></script>
+	<script src="../dist/js/dashboard-data.js"></script>
+	
+</body>
+
+</html>
