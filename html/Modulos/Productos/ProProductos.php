1f41dc227783961f74b27e6a0050416f452cd243
pistris <31252526+pistris@users.noreply.github.com>
2020-02-23 01:52:35 -0600

    Crud Productos

--- html/Modulos/Productos/ProProductos.php
+++ html/Modulos/Productos/ProProductos.php
@@ -7,34 +7,34 @@ $conexion = retornarConexion();
 switch ($_GET['accion']) {
     case 'listar':
         $respuesta = mysqli_query($conexion, "select 
-                                                 pro.codigo as codigo,
-                                                 pro.descripcion descripcion,
-                                                 cat.descripcion descripcioncategoria,
-                                                 precio
-                                              from productos as pro
-                                              join categorias as cat on cat.codigo=pro.codigocategoria");
+                                                 pro.intProducto as codigo,
+                                                 pro.varDescripcion as descripcion,
+                                                 cat.varDescripcion as descripcioncategoria,
+                                                 pro.dobPrecio as precio
+                                              from tCatProducto as pro
+                                              join tCatCategoriaProducto as cat on cat.intCategoriaProducto=pro.intProducto");
         $resultado = mysqli_fetch_all($respuesta, MYSQLI_ASSOC);
         echo json_encode($resultado);
         break;
     case 'agregar':
-        $respuesta = mysqli_query($conexion, "insert into productos(descripcion,precio,codigocategoria) values ('$_POST[descripcion]',$_POST[precio],$_POST[codigocategoria])");
+        $respuesta = mysqli_query($conexion, "insert into tCatProducto(varDescripcion,dobPrecio,intCategoriaProducto) values ('$_POST[descripcion]',$_POST[precio],$_POST[codigocategoria])");
         echo json_encode($respuesta);
         break;
     case 'recuperar':
-        $respuesta = mysqli_query($conexion, "select codigo, descripcion, precio, codigocategoria from productos where codigo=$_POST[codigo]");
+        $respuesta = mysqli_query($conexion, "select intProducto, varDescripcion, dobPrecio, intCategoriaProducto from tCatProducto where intProducto=$_POST[codigo]");
         $resultado = mysqli_fetch_all($respuesta, MYSQLI_ASSOC);
         echo json_encode($resultado);
         break;
     case 'borrar':
-        $respuesta = mysqli_query($conexion, "delete from productos where codigo=" . $_POST['codigo']);
+        $respuesta = mysqli_query($conexion, "delete from tCatProducto where intProducto=" . $_POST['codigo']);
         echo json_encode($respuesta);
         break;
     case 'modificar':
-        $respuesta = mysqli_query($conexion, "update productos set descripcion='$_POST[descripcion]', precio=$_REQUEST[precio], codigocategoria=$_POST[codigocategoria] where codigo=$_POST[codigo]");
+        $respuesta = mysqli_query($conexion, "update tCatProducto set varDescripcion='$_POST[descripcion]', dobPrecio=$_REQUEST[precio], intCategoriaProducto=$_POST[codigocategoria] where intProducto=$_POST[codigo]");
         echo json_encode($respuesta);
         break;
     case 'listarcategorias':
-        $respuesta = mysqli_query($conexion, "select codigo, descripcion from categorias");
+        $respuesta = mysqli_query($conexion, "select intCategoriaProducto, varDescripcion from tCatCategoriaProducto");
         $resultado = mysqli_fetch_all($respuesta, MYSQLI_ASSOC);
         echo json_encode($resultado);
         break;
