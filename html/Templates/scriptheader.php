e84b8ba4c8d0353245797d925ee220a2877ba05b
pistris <31252526+pistris@users.noreply.github.com>
2020-02-23 16:05:26 -0600

    templateAdrian

---
+++ html/Templates/scriptheader.php
@@ -0,0 +1,14 @@
+
+    <link href="../vendors/vectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" type="text/css" />
+	
+	<link href="../vendors/apexcharts/dist/apexcharts.css" rel="stylesheet" type="text/css" />
+	
+    <!-- Toggles CSS -->
+    <link href="../vendors/jquery-toggles/css/toggles.css" rel="stylesheet" type="text/css">
+    <link href="../vendors/jquery-toggles/css/themes/toggles-light.css" rel="stylesheet" type="text/css">
+	
+	<!-- Toastr CSS -->
+    <link href="../vendors/jquery-toast-plugin/dist/jquery.toast.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/style.css" rel="stylesheet" type="text/css">
