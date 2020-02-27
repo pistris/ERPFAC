01cb4efcc81ff62fd757bd77703a69d2cc1ba936
pistris <31252526+pistris@users.noreply.github.com>
2020-02-26 21:29:16 -0600

    CONEXION

--- html/conexion.php
+++ html/conexion.php
@@ -1,10 +1,10 @@
 <?php
 
 function retornarConexion() {
-    $server="bdsistemaerp.cyfdhmrju78e.us-east-2.rds.amazonaws.com";
-    $usuario="adminerp";
-    $clave="RD4l2fwiczO7W87bFbhj";
-    $base="dbERP";
+    $server="bhuaagzhlgugk5u9beiw-mysql.services.clever-cloud.com";
+    $usuario="uqmqkkwu33ddsizp";
+    $clave="RZIZqzC0eIuZ901QyzPA";
+    $base="bhuaagzhlgugk5u9beiw";
     return new PDO("mysql:dbname=$base;host=$server", "$usuario","$clave", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
 }
 
